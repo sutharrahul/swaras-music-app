237a4238793830ca74489785c3b50ee60e2962dd
Rahul suthar <sutharrahul119@gmail.com>
2025-05-28 12:13:42 +0530

    loading skeleton of music list create

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -2,12 +2,14 @@ import MusicPlayer from "@/components/MusicPlayer";
 import React from "react";
 import { truncateByLetters } from "../utils/truncateByLetters";
 import PlayList from "@/components/PlayList";
+import LoadingSkeleton from "@/components/LoadingSkeleton";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-between items-center h-full py-4">
       {/* play-list */}
 
+      <LoadingSkeleton />
       <PlayList />
       <MusicPlayer />
     </div>
---
+++ src/components/LoadingSkeleton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function LoadingSkeleton() {
+  return (
+    <div className="space-y-1 animate-pulse space-x-4 mx-auto">
+      <div className="flex items-center gap-5 py-3 px-2">
+        <div className="size-12 rounded bg-gray-200"></div>
+
+        <div className="flex-1 grid grid-cols-3 gap-4">
+          <div className="h-2 w-56 rounded  bg-gray-200"></div>
+          <div className="h-2 w-28 rounded  bg-gray-200"></div>
+          <div className="h-2 w-16 rounded  bg-gray-200"></div>
+        </div>
+
+        <div className="h-2 w-8 rounded  bg-gray-200"></div>
+      </div>
+    </div>
+  );
+}
