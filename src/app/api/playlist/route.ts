c2ea12f550a46e43b9ab20082bf2dd7dc16d9c49
Rahul suthar <sutharrahul119@gmail.com>
2025-06-12 21:20:51 +0530

    implement POST route to create playlist for user

---
+++ src/app/api/playlist/route.ts
@@ -0,0 +1,40 @@
+import { ApiResponce } from "@/app/utils/ApiResponse";
+import dbConnect from "@/lib/dbConnection";
+import PlaylistModel from "@/model/PlaylistModelSchema";
+
+export async function POST(request: Request) {
+  dbConnect();
+  try {
+    const { userId, songId } = await request.json();
+
+    const user = await PlaylistModel.findOne({ playlistUser: userId });
+
+    if (!user) {
+      const savedUser = new PlaylistModel({
+        playlistUser: userId,
+        playListSong: [songId],
+      });
+
+      const saveResponse = await savedUser.save();
+
+      return ApiResponce.success("User and Playlist add", saveResponse, 200);
+    }
+
+    const songInPlaylist = await PlaylistModel.findOne({
+      playlistUser: userId,
+      playListSong: songId,
+    });
+    if (songInPlaylist) {
+      return ApiResponce.error("Song is already exist in playlist", 409);
+    }
+
+    user?.playListSong.push(songId);
+    const saveResponse = await user.save();
+    console.log("save response", saveResponse);
+
+    return ApiResponce.success("Song add to playlist", saveResponse, 200);
+  } catch (error) {
+    console.log("faild add song to playlist");
+    return ApiResponce.error("Faild add song to playlist", 500);
+  }
+}
