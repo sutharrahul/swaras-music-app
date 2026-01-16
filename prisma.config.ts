fcb7051ac914f623588a239b23692ea39e4fe7e9
Rahul Suthar <sutharrahul119@gmail.com>
2026-01-16 14:13:13 +0530

    prisma config update

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
   },
   engine: 'classic',
   datasource: {
-    url: env('DATABASE_URL'),
+    url: env('DATABASE_URL') || 'postgresql://placeholder:placeholder@localhost:5432/placeholder',
   },
 });
